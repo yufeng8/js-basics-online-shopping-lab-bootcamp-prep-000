53b90c9de7cd170dcc452a353c4f515417001170
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-12 03:51:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,10 +78,10 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber) {
-    console.log("Sorry, we don't have a credit card on file for you.")
-  }
-  else {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
     cart.length = 0
+  }
+  else {
+    console.log("Sorry, we don't have a credit card on file for you.")
   }
 }
